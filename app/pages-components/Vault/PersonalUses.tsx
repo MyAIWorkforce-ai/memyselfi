409341965f40cefdd61851a21b39e82a1a443315
Ch Abdullah Shafique <abdullahshafique319@gmail.com>
2026-02-16 16:38:34 +0500

    update image path for vault page

--- app/pages-components/Vault/PersonalUses.tsx
+++ app/pages-components/Vault/PersonalUses.tsx
@@ -28,7 +28,7 @@ export const PersonalUses = () => {
           <Hovering>
             <div className="w-full sm:w-[370px] h-[300px] flex justify-center md:justify-start shrink-0 mt-6 md:mt-0">
               <img
-                src="/Vault/Personal.svg"
+                src="/Vault/personal.svg"
                 alt="Right Inbox"
                 className="object-cover clear-both mix-blend-multiply rounded-[15px] drop-shadow-[0_0_10px_rgba(255,239,67,0.8)]"
               />
